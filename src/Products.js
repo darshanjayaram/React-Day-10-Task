9487a37fb3665f29a13b718de24dc4e98fadf4a9
darshanjayaram <darshansj.darshu@gmail.com>
2025-01-08 23:26:04 +0530

    First Commit

---
+++ src/Products.js
@@ -0,0 +1,55 @@
+
+export default [
+    {
+        "id": 1,
+        "title": "iPhone 16 Plus",
+        "description": "An apple mobile which is nothing like apple",
+        "price": 899000,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "stock": 20,
+        "brand": "Apple",
+        "category": "smartphones",
+        "thumbnail": "https://inventstore.in/wp-content/uploads/2024/07/63.webp",
+        "images": "https://inventstore.in/wp-content/uploads/2024/07/63.webp"
+    },
+    {
+        "id": 2,
+        "title": "Apple iPhone 15 Pro Max",
+        "description": "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip",
+        "price": 149900,
+        "discountPercentage": 1000,
+        "rating": 4.44, //---------
+        "stock": 30,
+        "brand": "Apple",
+        "category": "smartphones",
+        "thumbnail": "https://inventstore.in/wp-content/uploads/2023/09/1-8.webp",
+        "images": 'https://inventstore.in/wp-content/uploads/2023/09/1-8.webp'
+    },
+    {
+        "id": 3,
+        "title": "iPad mini",
+        "description": "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip",
+        "price": 49900,
+        "discountPercentage": 1200,
+        "rating": 4.09,
+        "stock": 36,
+        "brand": "Apple",
+        "category": "smartphones",
+        "thumbnail": "https://inventstore.in/wp-content/uploads/2024/10/75.webp",
+        "images": 'https://inventstore.in/wp-content/uploads/2024/10/75.webp'
+    },
+    {
+        "id": 4,
+        "title": "MacBook Air M3 Chip – 15″",
+        "description": "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip",
+        "price": 116900,
+        "discountPercentage": 1800,
+        "rating": 4.3,
+        "stock": 120,
+        "brand": "Apple",
+        "category": "smartphones",
+        "thumbnail": "https://inventstore.in/wp-content/uploads/2024/03/MacBook_Air_13_in_M3_Space_Grey_PDP_Image_Position_1__en-IN-scaled.webp",
+        "images": 'https://inventstore.in/wp-content/uploads/2024/03/MacBook_Air_13_in_M3_Space_Grey_PDP_Image_Position_1__en-IN-scaled.webp'
+    }
+]
